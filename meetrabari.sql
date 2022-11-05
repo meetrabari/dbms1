eef91f5069d8094ae458423fca292347518313b8
meetrabari <117504551+meetrabari@users.noreply.github.com>
2022-11-05 12:09:02 +0530

    Create meetrabari.sql

---
+++ meetrabari.sql
@@ -0,0 +1,12 @@
+create table student13 (s_no number (5),s_name varchar (10),s_school varchar (10),s_hobby varchar (20),s_std number (10),s_gender varchar(10));
+insert into student13 values(1,'ram','sanklp','cricket',12,'male');
+insert into student13 values(2,'laxman','sanklp','cricket',12,'male');
+insert into student13 values(3,'yudhisthir','sanklp','cricket',12,'male');
+insert into student13 values(4,'jambuvan','sanklp','cricket',12,'male');
+insert into student13 values(5,'bharat','sanklp','cricket',12,'male');
+insert into student13 values(6,'angat','sanklp','cricket',12,'male');
+insert into student13 values(7,'jatayu','sanklp','cricket',12,'male');
+insert into student13 values(8,'ravan','sanklp','cricket',12,'male');
+insert into student13 values(9,'kumbhkaran','sanklp','cricket',12,'male');
+insert into student13 values(10,'vibhishan','sanklp','cricket',12,'male');
+select*from student13;
